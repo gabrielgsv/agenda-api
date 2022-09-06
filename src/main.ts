5070a1aaffeed962e22a97e30a6c38060f1e2bfd
gabrielgsv <gabriel.soares.vilela@gmail.com>
2022-09-06 14:11:02 -0300

    add swagger to api doc

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -13,6 +14,15 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Agenda Api')
+    .setDescription('agenda app api')
+    .setVersion('1.0')
+    .addTag('User', 'Auth')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(4000);
 }
