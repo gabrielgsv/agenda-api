33287e57001d3da123d819b25198850c44dbd936
gabrielgsv <gabriel.soares.vilela@gmail.com>
2022-09-06 14:10:41 -0300

    add update delete and get

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,11 +3,12 @@ import { Prisma } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly prisma: PrismaService) { }
+  constructor(private readonly prisma: PrismaService) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     const data: Prisma.UserCreateInput = {
@@ -25,5 +26,37 @@ export class UserService {
 
   findByEmail(email: string) {
     return this.prisma.user.findUnique({ where: { email } });
+  }
+
+  async getAll() {
+    const allUsers = await this.prisma.user.findMany();
+
+    await allUsers.forEach((user, index) => {
+      user.password = undefined;
+      allUsers[index] = user;
+    });
+
+    return allUsers;
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    await this.prisma.user.update({
+      where: {
+        id,
+      },
+      data: updateUserDto,
+    });
+
+    return `This action updates a #${id} user`;
+  }
+
+  async remove(id: number) {
+    await this.prisma.user.delete({
+      where: {
+        id,
+      },
+    });
+
+    return `Remove user with id: ${id}`;
   }
 }
