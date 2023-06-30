1b8101a9f3586289cef81205ea595692a9bce1dd
gabrielgsv <gabriel.soares.vilela@gmail.com>
2023-06-30 15:58:11 -0300

    hotfix cors

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  const options = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     preflightContinue: false,
     optionsSuccessStatus: 204,
     credentials: true,
-  });
+  };
+
+  app.enableCors(options);
 
   // Pipes
   app.useGlobalPipes(
